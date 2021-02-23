512f2227ee03d691927e536504aa3181c6b686bb
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-02-23 16:15:04 -0500

    order schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,12 @@
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
 
+CREATE TABLE parties (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
 CREATE TABLE candidates (
   id INTEGER PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
@@ -8,10 +14,4 @@ CREATE TABLE candidates (
   industry_connected BOOLEAN NOT NULL,
   party_id INTEGER UNSIGNED,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
-);
-
-CREATE TABLE parties (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
 );
